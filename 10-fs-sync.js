8d4da37f250f20bbad9b7d616e3a9406e2e92e14
Raffaele Di Martino <raf@raffaeles-mbp.home>
2023-02-12 15:31:21 +0000

    fs-module syncronus

--- 10-fs-sync.js
+++ 10-fs-sync.js
@@ -3,7 +3,7 @@
 // fs.readFileSync
 
 // descructuring
-const {readFileSync, writeFileSync, writeFile} = require("fs")
+const {readFileSync, writeFileSync} = require("fs")
 
 const first = readFileSync("./content/first.txt", "utf-8");
 const second = readFileSync("./content/second.txt", "utf-8");
