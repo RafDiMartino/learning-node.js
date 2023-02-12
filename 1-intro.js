7c889b5fa526aa46662a2c2d75582c353ed5fe8f
Raffaele Di Martino <raf@raffaeles-mbp.home>
2023-02-12 14:31:37 +0000

    node basic export/require

---
+++ 1-intro.js
@@ -0,0 +1,9 @@
+const ammount = 12;
+
+if (ammount < 10) {
+    console.log("small number");
+} else {
+    console.log("large number");
+}
+
+console.log(`hey it's my first node app!!!`)
---
+++ app.js
@@ -0,0 +1,12 @@
+// CommonJs, every file is module (by default)
+// Modules - Encapsulating Code (only share minimum)
+const names = require('./4-names')
+const sayHi = require('./5-utils')
+
+const data = require('./6-alternative-flavour')
+require("./7-mind-granade")
+// sayHi("Raf")
+// sayHi(names.john)
+// sayHi(names.peter)
+
+// console.log(module)
